d006bb48ecfab223ea3b114e20730e1e4dd904cb
인사팀 공용 <E7035@aes.hkmg.global>
2017-08-24 14:26:53 +0900

    nested class 실습, 접근 제한자 공부, 결국 접근이라는 것은 소스코드상에서 쓸수 있냐 없냐의 차이, public이 아닌이상 다른 패키지에서도 소스코드 상에서 쓸수가 없었음.

--- nested/main/NestedMain.java
+++ nested/main/NestedMain.java
@@ -9,6 +9,16 @@ public class NestedMain {
 		
 		Outer.insMemberCls a = ot.new insMemberCls(); //public 제한자를 class와 method에 붙였기에 현재 다른 package에서도 접근이 가능한 것임.
 		//접근이 가능하다? -> 소스코드상에서 쓸수 있다는 것. 이클립스 자동으로 뜨는것. 
+		a.method1();
+		a.field1 = 777;
+		
+		
+		Outer.sttMemberCls b = new Outer.sttMemberCls(); //static member class 
+		b.field1 = 777;
+		Outer.sttMemberCls.field2 = 7777;
+		
+		ot.methodForLocalCls(); //이 메소드를 호출하면 메소드에 정의된 로컬 클래스가 생성되고 사라지겠지. 
+		
 		
 	}
 
