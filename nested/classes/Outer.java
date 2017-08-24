f89ee8d5f2683fa3cc0d3d2d8fe1daa0c9754434
인사팀 공용 <E7035@aes.hkmg.global>
2017-08-24 14:25:10 +0900

    nested class 실습, 접근 제한자 공부, 결국 접근이라는 것은 소스코드상에서 쓸수 있냐 없냐의 차이, public이 아닌이상 다른 패키지에서도 소스코드 상에서 쓸수가 없었음.

--- nested/classes/Outer.java
+++ nested/classes/Outer.java
@@ -7,23 +7,23 @@ public class Outer {
 	//instance member class : static이 붙었나 안붙었나 차이 : outer 객체를 생성뒤 만들 수 있다.
 	public class insMemberCls{
 		public insMemberCls() {}
-		int field1;
+		public int field1;
 		//static int field2; //static class가 아니기에 static 변수, 메소드 쓸 수 없다.
-		void method1() {}
+		public void method1() {}
 		//static void method2() {}
 	}
 	
 	//static member class 
-	static class sttMemberCls{
+	public static class sttMemberCls{
 		public sttMemberCls() {}
-		int field1;
-		static int field2; //static member class 이기에 static 가능 
+		public int field1;
+		public static int field2; //static member class 이기에 static 가능 
 		void method1() {}
 		static void method2() {}
 	}
 	
 	//method for study local class
-	void methodForLocalCls() {
+	public void methodForLocalCls() {
 		class localCls{
 			localCls(){} //얘들은 어차피 메소드 안에 있는 class이므로 접근 제한자가 필요없음.
 			int field1;
